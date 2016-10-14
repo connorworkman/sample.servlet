e93041a6048509cf55e9eb4f6724da042285e427
Connor Workman <connor.workman@kibocommerce.com>
2016-10-14 14:28:45 -0500

    Changed bg color

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("<style>body{background-color:#FFFFFF}</style><body><h1><font color=green>Surprise!</font></h1>"
+        response.getWriter().print("<style>body{background-color:#0000FF}</style><body><h1><font color=green>Surprise!</font></h1>"
                                    + "It works." + "<br><img src='http://connorworkman.com/img/sample_rate_changes.gif'></body>");
     }
 
